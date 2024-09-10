109d1d3b1d584ea0fe8ff4a0f9abead3378d7fc0
adogbeji <baka_1993@yahoo.co.uk>
2024-09-10 18:56:01 +0100

    Start generating random number

--- app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -3,5 +3,8 @@ package com.example.rockpaperscissors
 fun main() {
     var computerChoice = ""
     var playerChoice = ""
-    println("Rock, Paper or Scissors? ")
+    println("Rock, Paper or Scissors? Enter your choice!")
+    playerChoice = readln()
+
+    val randomNumber = (1..3).random()
 }
