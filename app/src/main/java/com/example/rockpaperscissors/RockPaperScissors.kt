7a31620ffcdfe9f33148e0813af1e9eefa5ccc69
adogbeji <baka_1993@yahoo.co.uk>
2024-09-26 14:51:21 +0100

    Create variable to store winner

--- app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -15,4 +15,13 @@ fun main() {
     } else if (randomNumber == 3) {
         computerChoice = "Scissors"
     }
+    println(computerChoice)
+
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
 }
